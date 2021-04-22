ed514ba1cb6ed64d458334aab1e077279ea4abe7
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-22 14:07:36 +0200

    Header basics finished

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -3,20 +3,28 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <header>
+            <header style={headerStyle}>
             <h1>The Manchester Guardian.</h1>
             <hr />
                 <p style={sectionOne} class='header-font'>London: Compiled and Published by K. VINER, No. 90, York Way.</p>
                 <hr />
-                <div style={flexContainer}>
-                    <div style={flexItem}>No. 1</div>
-                    <div style={flexItem}>Friday, April 23, 2021.</div>
-                    <div style={flexItem}>Price seven-pence</div>
+                <div style={flexContainer} class='header-font'>
+                    <div style={flexItem}><p>No. 1</p></div>
+                    <div style={flexItem}><p style={{textAlign: 'center'}}>Friday, April 23, 2021.</p></div>
+                    <div style={flexItem}><p style={{textAlign: 'right'}}>Price seven-pence</p></div>
                 </div>
                 <hr />
             </header>
         </div>
     )
+}
+
+
+const headerStyle = {
+    width: '90%',
+    margin: 'auto',
+    color: 'transparent',
+    textShadow: '0 0 1px rgba(0,0,0,0.9)',
 }
 
 const sectionOne = {
@@ -30,6 +38,8 @@ const sectionOne = {
 const flexContainer = {
     display: 'flex',
     flexWrap: 'nowrap',
+    fontSize: '20px',
+    fontWeight: 'bold',
 }
 
 const flexItem = {

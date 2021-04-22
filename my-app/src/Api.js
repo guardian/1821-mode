b22ca9220af787978d2765a2e9435e91b871b075
Mario Savarese <mario.savarese@theguardian.com>
2021-04-22 13:57:26 +0100

    fix api call
    Co-authored-by: Samantha Gottlieb <samanthagottlieb31@gmail.com>

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -1,35 +1,27 @@
-import Guardian from 'guardian-js';
+// import Guardian from 'guardian-js';
+import { useState } from 'react';
 const apiKey = 'gnm-hackday-21'
 
-const api = new Guardian(apiKey, false);
-const apiUrl = "https://content.guardianapis.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target"
+//const api = new Guardian(apiKey, false);
+const apiUrl = "https://content.guardianapis.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target?show-fields=body"
 
 async function getNews() {
-    await fetch(apiUrl + "?api-key="+ apiKey)
-    .then(res => {
-        return  res.json()
-    })
-  
+    return await fetch(apiUrl + "&api-key="+ apiKey)
+    .then(res =>  res.json())
 }
-
-  //  const responseData = await response.json();
-  //  return responseData
-
-
 
 function Api() {
     
-    // // const test = api.item.getById('business/2014/feb/18/uk-inflation-falls-below-bank-england-target')
-    //             .then((response) => {
-    //              let text = response.body;
-    //             return text
-    //     });   
-    
-    let test = getNews();
-    console.log(test)
+    const [result, setResult] = useState(0)
+    getNews().then(n => {
+        let stringified = JSON.stringify(n)
+        let obj = JSON.parse(stringified)
+        console.log(obj)
+        setResult(obj.response.content.webTitle + obj.response.content.fields.body)
+    })
     return (
         <div>
-            <h1>${test}</h1>
+            <h1>{result}</h1>
         </div>
     )
 } 

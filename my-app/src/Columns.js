500363f5a8a069531284ef34bd2ae3110149d001
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-22 20:34:31 +0200

    Fourth ad

--- my-app/src/Columns.js
+++ my-app/src/Columns.js
@@ -11,6 +11,9 @@ function Columns() {
             <img src="https://audioxide-wiki.neocities.org/Images/ad2.png" alt="Old fashioned advert" />
             <Api article="environment/2021/apr/22/boris-johnson-urges-leaders-to-get-serious-at-climate-summit" />
             <img src="https://audioxide-wiki.neocities.org/Images/ad3.png" alt="Old fashioned advert" />
+            <Api article="technology/2021/apr/22/ransomware-hackers-steal-plans-upcoming-apple-products" />
+            <img src="https://audioxide-wiki.neocities.org/Images/ad4.png" alt="Old fashioned advert" />
+
         </div>
     )
 }

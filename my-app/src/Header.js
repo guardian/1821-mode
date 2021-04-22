91f1446d3583f3a2a1ee93fdef5bc5c43f761100
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-22 14:17:00 +0200

    Header style tweaks

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -9,8 +9,8 @@ function Header() {
                 <p style={sectionOne} class='header-font'>London: Compiled and Published by K. VINER, No. 90, York Way.</p>
                 <hr />
                 <div style={flexContainer} class='header-font'>
-                    <div style={flexItem}><p>No. 1</p></div>
-                    <div style={flexItem}><p style={{textAlign: 'center'}}>Friday, April 23, 2021.</p></div>
+                    <div style={flexItem}><p>No. 1.</p></div>
+                    <div style={flexItem}><p style={{textAlign: 'center'}}>FRIDAY, APRIL 23, 2021.</p></div>
                     <div style={flexItem}><p style={{textAlign: 'right'}}>Price seven-pence</p></div>
                 </div>
                 <hr />

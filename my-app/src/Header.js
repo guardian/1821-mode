45a32ce566a0b270f000fa5256abfe4ba68201af
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-22 12:35:53 +0200

    Universal styling and early heading features

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -3,9 +3,21 @@ import React from 'react'
 function Header() {
     return (
         <div>
+            <header>
             <h1>The Manchester Guardian.</h1>
+            <hr />
+                <p style={divStyle}>Printed in London</p>
+                <hr />
+                    <p>Date</p>
+                    <hr />
+            </header>
         </div>
     )
 }
+
+const divStyle = {
+    color: 'blue',
+    fontSize: '20px'
+};
 
 export default Header

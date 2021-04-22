98a50b7ac683188399eaae14a284dd5e37570b7c
Mario Savarese <mario.savarese@theguardian.com>
2021-04-22 17:16:03 +0100

    add [] to useeffect

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -5,7 +5,7 @@ import ReactHtmlParser from 'react-html-parser';
 const apiKey = 'fa7a2b96-0cb9-4da8-b684-c33cad253d0d'
 
 //const api = new Guardian(apiKey, false);
-const apiUrl = "https://content.guardianapis.com/uk-news/2021/apr/22/45bn-covid-recovery-plan-at-heart-of-scottish-labour-manifesto?show-fields=body"
+const apiUrl = "https://content.guardianapis.com/uk-news/2021/apr/22/minister-apologises-for-black-and-asian-war-dead-commemoration-failures?show-fields=body"
 
 async function getNews() {
     return await fetch(apiUrl + "&api-key="+ apiKey)
@@ -14,14 +14,14 @@ async function getNews() {
 
 function Api() {
     
-    const [result, setResult] = useState(0)
+    const [result, setResult] = useState([])
 
     useEffect(() => {
     getNews().then(n => {
        // var title = "<h2>" + n.response.content.webTitle + "</h2>"
         //var body = n.response.content.fields.body
         setResult(n)
-    })})
+    })}, [])
     return (
         <div>
             <h2>{ReactHtmlParser(result.response.content.webTitle)}</h2>

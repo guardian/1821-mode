fc3eece16729e42f6166bb0c3f9fc78e3ab24da7
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-22 12:21:01 +0200

    Clear styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,7 @@ import Header from './Header.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
           <Header />
-      </header>
     </div>
   );
 }

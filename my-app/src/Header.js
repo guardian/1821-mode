5cfdc4e0b22b0504af8b46701fefba1a49b4fe08
Ara_cho <jarjeb@gmail.com>
2021-04-22 12:07:26 +0100

    playing with the header style

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -6,18 +6,32 @@ function Header() {
             <header>
             <h1>The Manchester Guardian.</h1>
             <hr />
-                <p style={divStyle}>Printed in London</p>
+                <p style={divStyle} class='header-font'>printed in london</p>
                 <hr />
-                    <p>Date</p>
+                <div class={subHeaderStyle}>
+                    <p>No. 1.</p>
+                    <p style={divStyle} class='header-font'>FRIDAY, APRIL 23, 2021.</p>
+                    <p style={divStyle}>price seven-pence</p>
+                </div>
                     <hr />
+                
             </header>
         </div>
     )
 }
 
 const divStyle = {
-    color: 'blue',
-    fontSize: '20px'
+    // color: 'blue',
+    fontSize: '20px',
+    textAlign: 'center',
+    textTransform: 'uppercase',
 };
+
+const subHeaderStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    // alignItems: 'center',
+    color: 'blue'
+}
 
 export default Header

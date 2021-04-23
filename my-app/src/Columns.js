d109b89200444d299346c3d567fff32238f07847
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-23 12:44:27 +0200

    Steve Bell cartoon

--- my-app/src/Columns.js
+++ my-app/src/Columns.js
@@ -8,13 +8,17 @@ function Columns() {
             <Api article="artanddesign/2021/apr/23/damn-this-is-a-caravaggio-the-inside-story-of-an-old-master-found-in-spain" percentage="40" thumbnail="yes" />
             <hr />
             <img src="https://audioxide-wiki.neocities.org/Images/ad1.png" alt="Old fashioned advert" />
+            <hr />
             <Api article="technology/2021/apr/22/signal-founder-i-hacked-police-phone-cracking-tool-cellebrite" percentage="70" />
             <hr />
             <img src="https://audioxide-wiki.neocities.org/Images/ad2.png" alt="Old fashioned advert" />
+            <hr />
             <Api article="environment/2021/apr/22/boris-johnson-urges-leaders-to-get-serious-at-climate-summit" percentage="60" />
             <hr />
-            <img src="https://audioxide-wiki.neocities.org/Images/ad4.png" alt="Old fashioned advert" />
+            <img src="https://audioxide-wiki.neocities.org/Images/stevebell.png" alt="Old fashioned advert" />
+            <hr />
             <img src="https://audioxide-wiki.neocities.org/Images/ad3.png" alt="Old fashioned advert" />
+            <hr />
             <Api article="technology/2021/apr/22/ransomware-hackers-steal-plans-upcoming-apple-products" thumbnail="yes" />
         </div>
     )

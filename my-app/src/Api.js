5081501417ddb10619135f35a39b9ad2e9aa6bf6
Mario Savarese <mario.savarese@theguardian.com>
2021-04-29 12:19:20 +0100

    format document

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -1,70 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import ReactHtmlParser from 'react-html-parser';
+import React, { useEffect, useState } from "react";
+import ReactHtmlParser from "react-html-parser";
 
-const apiKey = 'fa7a2b96-0cb9-4da8-b684-c33cad253d0d'
-const guardianApi = 'https://content.guardianapis.com/'
-
+const apiKey = "fa7a2b96-0cb9-4da8-b684-c33cad253d0d";
+const guardianApi = "https://content.guardianapis.com/";
 
 // Fields to show in the API response. Documentation can be found here:
 // https://open-platform.theguardian.com/documentation/item
-const articleFields = ['body', 'thumbnail']
-const showFields = (fields) => fields.join(",")
-const fieldsToShow = showFields(articleFields)
+const articleFields = ["body", "thumbnail"];
+const showFields = (fields) => fields.join(",");
+const fieldsToShow = showFields(articleFields);
 
 async function getNews(id) {
-    return await fetch(guardianApi + id + "?show-fields="+ fieldsToShow +"&api-key="+ apiKey)
-    .then(res =>  res.json())
+  return await fetch(
+    guardianApi + id + "?show-fields=" + fieldsToShow + "&api-key=" + apiKey
+  ).then((res) => res.json());
 }
 
 const trimArticle = (body, percent) => {
-    return body.substring(0, (percent * body.length) / 100 )
-}
+  return body.substring(0, (percent * body.length) / 100);
+};
 
 const continueReading = (articleID) => {
-    return `<p class="continue-reading"><a href="${articleID}">Continue reading...</a></p>`
-}
+  return `<p class="continue-reading"><a href="${articleID}">Continue reading...</a></p>`;
+};
 
 const showThumbnail = (thumbnailUrl) => {
-    return `<div class="filtered" style="background-image:url('${thumbnailUrl}')" alt="Thumbnail"></div>`
-}
+  return `<div class="filtered" style="background-image:url('${thumbnailUrl}')" alt="Thumbnail"></div>`;
+};
 
 function Api(props) {
-    
-    const [title, setTitle] = useState(0)
-    const [body, setBody] = useState(0)
-    const [thumbnail, setThumbnail] = useState(0)
+  const [title, setTitle] = useState(0);
+  const [body, setBody] = useState(0);
+  const [thumbnail, setThumbnail] = useState(0);
 
-    const link = "https://www.theguardian.com/" + props.article
+  const link = "https://www.theguardian.com/" + props.article;
 
+  // TODO: replace modern expressions with 19th century equivalents.
+  useEffect(() => {
+    getNews(props.article).then((n) => {
+      // extract content from the API response
+      const title = n.response.content.webTitle;
+      const bodyResponse = n.response.content.fields.body;
+      const thumbnailResponse = n.response.content.fields.thumbnail;
+      const body = props.percentage
+        ? trimArticle(bodyResponse, props.percentage) + continueReading(link)
+        : bodyResponse;
+      const thumbnail =
+        props.thumbnail === "yes" ? showThumbnail(thumbnailResponse) : "";
 
-    // TODO: replace modern expressions with 19th century equivalents. 
-    useEffect(() => {
-    getNews(props.article).then(n => {
+      setTitle(title);
+      setBody(body);
+      setThumbnail(thumbnail);
+    });
+  }, []);
 
-        // extract content from the API response
-        const title = n.response.content.webTitle
-        const bodyResponse = n.response.content.fields.body
-        const thumbnailResponse = n.response.content.fields.thumbnail
-        const body = props.percentage ? trimArticle(bodyResponse, props.percentage) + continueReading(link) : bodyResponse
-        const thumbnail = props.thumbnail === "yes" ? showThumbnail(thumbnailResponse) : ""
+  return (
+    <div>
+      <h2>
+        <a href={link}>{title}</a>
+      </h2>
+      {ReactHtmlParser(thumbnail)}
+      <p class="story-copy">{ReactHtmlParser(body)}</p>
+    </div>
+  );
+}
 
-        setTitle(title)
-        setBody(body)
-        setThumbnail(thumbnail)
-            
-    })}, [])
-    
-
-    return (
-        <div>
-            <h2><a href={link}>{title}</a></h2>
-            {ReactHtmlParser(thumbnail)}
-            <p class="story-copy">{ReactHtmlParser(body)}</p>
-        </div>
-   
-    )
-} 
-
-export default Api
-export { getNews }
-export { apiKey, guardianApi }
+export default Api;
+export { getNews };
+export { apiKey, guardianApi };

f81674233c6d4cffd727ba87218117e0e876e511
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-29 19:53:42 +0200

    Update Columns.js

--- my-app/src/Columns.js
+++ my-app/src/Columns.js
@@ -7,6 +7,11 @@ import HopeIsPower from './Images/HopeIsPower.png';
 import HarvardsTeethAd from './Images/HarvardsTeethAd.png';
 import CarrossierAd from './Images/CarrossierAd.png';
 import LifebuoySoapAd from './Images/LifebuoySoapAd.png';
+import PearsSoapAd from './Images/PearsSoapAd.png';
+import FrancisPrestonAd from './Images/FrancisPrestonAd.png';
+import HartSonPeardCoAd from './Images/HartSonPeardCoAd.png';
+import OrchestrionAd from './Images/OrchestrionAd.png';
+import PeerlessDictionaryAd from './Images/PeerlessDictionaryAd.png';
 
 function Columns() {
     return (
@@ -17,19 +22,22 @@ function Columns() {
              <hr />
             <Api article="technology/2021/apr/22/signal-founder-i-hacked-police-phone-cracking-tool-cellebrite" percentage="100" />
             <hr />
-            <img src={LifebuoySoapAd} alt="Old fashioned advert" />
+            <img src={CarrossierAd} alt="Old fashioned advert for a Carrossier" />
+            <img src={PeerlessDictionaryAd} alt="Old fashioned advert for the Peerless Dictionary" />
             <hr />
             <Api article="lifeandstyle/2020/jan/23/petflix-and-chill-does-your-dog-need-a-streaming-service" percentage="90" />
             <hr />
-            <img src={CarrossierAd} alt="Old fashioned advert" />
+            <img src={LifebuoySoapAd} alt="Old fashioned advert for Lifebuoy Soap" />
             <hr />
-            <img src={HarvardsTeethAd} alt="Old fashioned advert" />
+            <img src={HarvardsTeethAd} alt="Old fashioned advert Harvard's Teeth department store" />
             <hr />
             <Api article="fashion/2021/apr/12/hawaiian-shirts-fashion-trend-expert" />
             <hr />
-            <img src={PuritySaltAd} alt="Old fashioned advert" />
-            <hr />
             <Api article="technology/2021/apr/22/ransomware-hackers-steal-plans-upcoming-apple-products" />
+            <img src={FrancisPrestonAd} alt="Old fashioned advert for Francis Preston arms" />
+            <img src={HartSonPeardCoAd} alt="Old fashioned advert for communion plate manufacturers" />
+            <img src={OrchestrionAd} alt="Old fashioned advert for organ builders" />
+            <img src={PearsSoapAd} alt="Old fashioned advert for Pears' Soap" />
         </div>
     )
 }

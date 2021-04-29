2fd4148ea858a1d0b4c43f42c37accbce6facd54
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-04-29 12:36:55 +0200

    Internal images
    Fixed the image references so we're not calling them from an outside source any more. Turns out we can import them to different components, which keeps things nice and tidy.
    
    Have added a handful of fair use ads from Wikimedia Commons to test this though they may or may not make the cut.

--- my-app/src/Columns.js
+++ my-app/src/Columns.js
@@ -2,28 +2,34 @@ import React from 'react'
 
 import Api from './Api.js';
 
+import PuritySaltAd from './Images/PuritySaltAd.png';
+import HopeIsPower from './Images/HopeIsPower.png';
+import HarvardsTeethAd from './Images/HarvardsTeethAd.png';
+import CarrossierAd from './Images/CarrossierAd.png';
+import LifebuoySoapAd from './Images/LifebuoySoapAd.png';
+
 function Columns() {
     return (
         <div class="column-container">
-            <Api article="artanddesign/2021/apr/23/damn-this-is-a-caravaggio-the-inside-story-of-an-old-master-found-in-spain" percentage="60" thumbnail="yes" />
-            <hr />
-            <img src="https://audioxide-wiki.neocities.org/Images/stevebell.png" alt="Old fashioned advert" />
+            <Api article="artanddesign/2021/apr/23/damn-this-is-a-caravaggio-the-inside-story-of-an-old-master-found-in-spain" percentage="60" />
                 <hr />
-                <img src="https://lh3.googleusercontent.com/-CbwK5UcbEFmejBuRkNpkYNHzpaYoaEx9FdSiATrhAo72mMKXjV4k17ER9RxUb2OdBAv2nFG4BIhW-h2ZakopWu68IL8zgPqKS7A7pkaKd0x7j9yB-u0RBw1U3Mi9ISoAB1-ObNVgscDonL8KXzJpy9efJYbw6gdDUcC4R-8-xVSSuKv8CSOP3dQFS87Q8fy4cGWc7wJS2bOiSpiq75kSKr-BtBx4ESXZXDXiWYfehfPJiBkRomj1Mz30df-qPQ8o7i-bHPpNHy-BDr9YHgBSrQlxxUghWB6KZSq6w8MKrwHaQddZwpfp3mjyZqNNsNcWzI0NuhitHNPWfwUvkHHZThXW8B8L52AQe4GjlWv7bvWWIp1X5AW8qugdRFkY9xurOpYn3EUjBNBphoqJ9yV_JR7ykRJ6Ob4Adiq0CZcxQDB_gX7Mtj1beZqymMo4V2JS2KkgakD01x7RPQPhaoGN85xOW2lOa_5EwDCEjrmR0leH0OSJOGeuu7XnRqgeICHM-tX_BjgHJ8W5tqp5Htqjt0dPXdZo8Gbs805OTM6cn15mrx-HRJh3ZO2KNe00pyehONL7K4YUqgefehGuF3yPPAqLDe0K0-iWQP0AVaVRol0rxcdoKa3Uvu80UvOmIMNtxZHg8s80puWhU_0Y6-fVBdK60LpqkHPa1Q4kMM513zy7ef_NXeit5_cBxvidOmcstlsz5_aKgxZR_kte58woA=w810-h1167-no?authuser=0" alt="Old fashioned advert" />
-                <hr />
+            <a href="https://support.theguardian.com/" target="_blank"><img className="contribution-cta" src={HopeIsPower} alt="Old fashioned advert" /></a>
+             <hr />
             <Api article="technology/2021/apr/22/signal-founder-i-hacked-police-phone-cracking-tool-cellebrite" percentage="100" />
             <hr />
-            <img src="https://audioxide-wiki.neocities.org/Images/ad2.png" alt="Old fashioned advert" />
+            <img src={LifebuoySoapAd} alt="Old fashioned advert" />
             <hr />
             <Api article="lifeandstyle/2020/jan/23/petflix-and-chill-does-your-dog-need-a-streaming-service" percentage="90" />
             <hr />
-            <img src="https://audioxide-wiki.neocities.org/Images/ad1.png" alt="Old fashioned advert" />
+            <img src={CarrossierAd} alt="Old fashioned advert" />
             <hr />
-            <img src="https://lh3.googleusercontent.com/fife/ABSRlIoFILIMqKK-rfhPzvBGWHhyCvRK-PmzcUc-jlwXh2kff4gRIumV-Ds6bsJiLhi-sidzBC7Cs2konuR2hx4v975w7CkYm3G7kJBtnMsqvmpDBqVbF3grFqLM7t77tLvG9N2JY3U-BGj8g8rWY13mdivf66jvUSowwCeRkn3hmP9TNVxxoIFetW5HREDy0QLLKl2mrE2iVnRKtzBpFNXKvInVPp9cGfTSh1aTPBpMRoPDWIz2pH-G0yhIAgBHxVw8PjFYX6KThXXzDTU4TzKDfSvGGuR7iELcIZfJzdktN8OQx_mdmcTzr6k6fETvnaJOIZ1wvuQIX2pvaAxnR5_e_s26X-uVzjsDO3sQyWfT7DmLp6zT-YHBlBh8_hqoywQ9a8gUyflXTIwfOAPrZeb8RhA6Uu395xPK-rL8POVjcCtb75GVBP_h1gYKCMacOQradx90iVA-X8iuoBAOCCatnARIedPaqIyj4S77LFCvXh8N6ALnafkQnwfwIixM2Kir2eXC-6GQzoJJOwyeHaPM9f1VhuXU7esmT2bBvXNjVukp0Sp2JPiyRbN9nUOrwxibNXmyh4LBAwfn_IbbAyFSMiMaxD0O20etgeBRSmGALR0faFN3GBoSgXaifJdYFQe3eDGrBlfsmrcI-QZ53UTw2FcnG-8AdsJMCIdcFOEle3nWlxrfUnL7ENAal2Jm8iixK33f7iVNFLg=w613-h929-no?authuser=0" alt="Old fashioned advert" />
+            <img src={HarvardsTeethAd} alt="Old fashioned advert" />
             <hr />
             <Api article="fashion/2021/apr/12/hawaiian-shirts-fashion-trend-expert" />
             <hr />
-            <Api article="technology/2021/apr/22/ransomware-hackers-steal-plans-upcoming-apple-products" thumbnail="yes"/>
+            <img src={PuritySaltAd} alt="Old fashioned advert" />
+            <hr />
+            <Api article="technology/2021/apr/22/ransomware-hackers-steal-plans-upcoming-apple-products" />
         </div>
     )
 }

183e31eb5c76031b83dfa8032618292171f3fb41
Mario Savarese <mario.savarese@theguardian.com>
2021-04-29 11:21:14 +0100

    remove unused variables and tidy up code

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -1,16 +1,20 @@
-// import Guardian from 'guardian-js';
 import React, { useEffect, useState } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { Link, withRouter } from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 const apiKey = 'fa7a2b96-0cb9-4da8-b684-c33cad253d0d'
 const guardianApi = 'https://content.guardianapis.com/'
-//const api = new Guardian(apiKey, false);
+
+
+// Fields to show in the API response. Documentation can be found here:
+// https://open-platform.theguardian.com/documentation/item
+const articleFields = ['body', 'thumbnail']
+const showFields = (fields) => fields.join(",")
+const fieldsToShow = showFields(articleFields)
 
 async function getNews(id) {
-    return await fetch(guardianApi + id + "?show-fields=body,thumbnail&api-key="+ apiKey)
+    return await fetch(guardianApi + id + "?show-fields="+ fieldsToShow +"&api-key="+ apiKey)
     .then(res =>  res.json())
 }
 

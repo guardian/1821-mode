ce1430da70333180da4f12e1aa21057cc049ff17
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2021-05-03 00:43:44 +0100

    Create function to replace words in articles
    Words and replacements can be changed within replaceWords function

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -9,7 +9,18 @@ const continueReading = (articleID) => {
   return `<p class="continue-reading"><a href="${articleID}">Continue reading...</a></p>`;
 };
 
-// TODO: replace modern expressions with 19th century equivalents.
+// replace modern words/expressions with 19th century equivalents.
+const replaceWords = (articleBody) => {
+  const mapObj = {
+    you: "thou",
+    know: "knowest"
+  };
+  let articleOldStyle = articleBody.replaceAll(/you|know/g, (matched) => {
+    return mapObj[matched]
+  });
+
+  return articleOldStyle
+}
 
 function Api(props) {
   // extract content from the API response stored in all.json
@@ -17,9 +28,10 @@ function Api(props) {
   const link = "https://www.theguardian.com/" + articleLink;
   const title = props.article.response.content.webTitle;
   const bodyResponse = props.article.response.content.fields.body;
-  const body = props.percentage
+  const bodyTrimmed = props.percentage
     ? trimArticle(bodyResponse, props.percentage) + continueReading(link)
     : bodyResponse;
+  const body = replaceWords(bodyTrimmed);
   
   return (
     <div class="story">

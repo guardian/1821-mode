f22d6951c50a339146b2ea9ffbc0cb24cc2287bb
Ara_cho <jarjeb@gmail.com>
2021-05-04 23:49:05 +0100

    ctas

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,62 +1,122 @@
-import React from 'react'
-import GuardianLogo from './guardianLogo.js'
+import React from "react";
+import GuardianLogo from "./guardianLogo.js";
+import { Button, LinkButton } from "@guardian/src-button";
 
 function Header() {
-    const d = new Date();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const d = new Date();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    const dayOfTheWeek = days[d.getDay()].toUpperCase()
-    const month = months[d.getMonth()].toUpperCase()
-    const dayOfTheMonth = d.getDate()
-    const year =  d.getFullYear()
+  const dayOfTheWeek = days[d.getDay()].toUpperCase();
+  const month = months[d.getMonth()].toUpperCase();
+  const dayOfTheMonth = d.getDate();
+  const year = d.getFullYear();
 
-    return (
-            <header style={headerStyle}>
-            <div class="logo-styles">
-                    <GuardianLogo />
-                </div>
-            <hr />
-                <p style={sectionOne} className='header-part-one'>World Wide Web: Compiled and Published by K. VINER & CO, No. 90, York Way.</p>
-                <hr style={headerDivider} />
-                <div style={flexContainer} className='header-part-two'>
-                    <div style={flexItem}><p class="small-caps header-left">No. 54, 337.</p></div>
-                    <div style={flexItem}><p style={{textAlign: 'center'}} class="small-caps" >{dayOfTheWeek}, {month} {dayOfTheMonth}, {year}.</p></div>
-                    <div style={flexItem}><p style={{textAlign: 'right'}} class="small-caps header-right" ><a href="https://support.theguardian.com/subscribe?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode" style={supportLinks}>Subscribe</a> | <a href="https://support.theguardian.com/contribute?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode" style={supportLinks}>Contribute</a>.</p></div>                </div>
-                <hr style={headerDivider} />
-            </header>
-    )
+  return (
+    <header style={headerStyle}>
+      <div class="logo-styles">
+        <GuardianLogo />
+      </div>
+      <hr />
+      <p style={sectionOne} className="header-part-one">
+        World Wide Web: Compiled and Published by K. VINER & CO, No. 90, York
+        Way.
+      </p>
+      <hr style={headerDivider} />
+      <div style={flexContainer} className="header-part-two">
+        <div style={flexItem}>
+          <p class="small-caps header-left">No. 54, 337.</p>
+        </div>
+        <div style={flexItem}>
+          <p style={{ textAlign: "center" }} class="small-caps">
+            {dayOfTheWeek}, {month} {dayOfTheMonth}, {year}.
+          </p>
+        </div>
+        <div style={ctaContainerStyles}>
+          <LinkButton
+            href="https://support.theguardian.com/subscribe?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode"
+            priority="tertiary"
+            size="xsmall"
+            cssOverrides={ctaStyles}
+          >
+            Subscribe
+          </LinkButton>
+          <LinkButton
+            href="https://support.theguardian.com/contribute?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode"
+            priority="tertiary"
+            size="xsmall"
+            cssOverrides={ctaStyles}
+          >
+            Contribute
+          </LinkButton>
+          </div>
+      </div>
+
+      <hr style={headerDivider} />
+    </header>
+  );
 }
-
 
 const headerStyle = {
-    paddingBottom: '10px',
-}
+  paddingBottom: "10px",
+};
 
 const sectionOne = {
-    textAlign: 'center',
-    fontStyle: 'italic',
+  textAlign: "center",
+  fontStyle: "italic",
 };
 
 const flexContainer = {
-    display: 'flex',
-    flexWrap: 'nowrap',
-    fontWeight: 'bold',
-}
+  display: "flex",
+  flexWrap: "nowrap",
+  fontWeight: "bold",
+};
 
 const flexItem = {
-    flex: '1'
+  flex: "1",
+};
+
+const ctaContainerStyles = {
+    flex: "1",
+    textAlign: "right",
 }
 
 const headerDivider = {
-    border: '1.5px solid black',
-    backgroundColor: 'black',
-    borderRadius: '10px',
-}
+  border: "1.5px solid black",
+  backgroundColor: "black",
+  borderRadius: "10px",
+};
 
-const supportLinks = {
-    fontWeight: 'bold',
-    textDecoration: 'underline'
+const ctaStyles = {
+    color: 'black',
+    // fontFamily: 'IM Fell English SC, serif',
+    textAlign: 'center',
+    marginLeft: '0.2em',
+    '&:hover': {
+        backgroundColor: '#333333',
+        borderColor: '#333333',
+        color: 'white'
+    }
 }
-
-export default Header
+export default Header;

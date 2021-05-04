7d4b4a0513c20c66dac4fc33e323f8d4ee949a79
Ara_cho <jarjeb@gmail.com>
2021-05-04 12:20:52 +0100

    support links

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -16,8 +16,7 @@ function Header() {
                 <div style={flexContainer} className='header-part-two'>
                     <div style={flexItem}><p class="small-caps" >No. 68, 082.</p></div>
                     <div style={flexItem}><p style={{textAlign: 'center'}} class="small-caps" >{day + ", "+ month+" " + d.getDate()+", "+d.getFullYear()}</p></div>
-                    <div style={flexItem}><p style={{textAlign: 'right'}} class="small-caps" >Price varies</p></div>
-                </div>
+                    <div style={flexItem}><p style={{textAlign: 'right'}} class="small-caps" ><a href="https://support.theguardian.com/subscribe?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode" style={supportLinks}>Subscribe</a> | <a href="https://support.theguardian.com/contribute?acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%221821_mode%22%2C%22campaignCode%22%3A%221821_mode%22%7D&INTCMP=1821_mode" style={supportLinks}>Contribute</a></p></div>                </div>
                 <hr style={headerDivider} />
             </header>
     )
@@ -49,6 +48,10 @@ const flexItem = {
 
 const headerDivider = {
     border: '2px solid gray',
+}
+
+const supportLinks = {
+    fontWeight: 'bold',
 }
 
 export default Header
